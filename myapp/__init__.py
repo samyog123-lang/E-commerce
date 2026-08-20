1097874346d26d5cd552c54c0db0ff3deb66c014
Samyog Panthi <samyogpanthee238@gmail.com>
2026-08-20 08:27:06 +0545

    Update __init__.py

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager   
+from .views import views
+from .auth import auth
+from .admin import admin as admin_bp
 
 db = SQLAlchemy()
 login_manager = LoginManager()
-login_manager.login_view = 'auth.login'  # points to auth blueprint
+login_manager.login_view = 'auth.login'  
 
 def create_app():
     app = Flask(__name__)
@@ -12,11 +15,9 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Initialize extensions
     db.init_app(app)
     login_manager.init_app(app)
 
-    # Register blueprints
     from .views import views
     from .auth import auth
     from .admin import admin as admin_bp
@@ -25,15 +26,14 @@ def create_app():
     app.register_blueprint(auth)
     app.register_blueprint(admin_bp)
 
-    # User loader
-    from .models import Customer, Order  # Import Order too so tables exist
+
+    from .models import Customer, Order 
 
     @login_manager.user_loader
     def load_user(user_id):
         return Customer.query.get(int(user_id))
 
-    # -------------------- ADD THIS --------------------
-    # Create all tables if they don't exist yet
+
     with app.app_context():
         db.create_all()
     # ---------------------------------------------------
